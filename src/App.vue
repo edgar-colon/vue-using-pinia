849651761531bfb0f10671fae762679bdb7546a5
Edgar Colon <ecolon@lumston.com>
2023-04-24 21:20:07 -0600

    Example using props and emits

---
+++ src/App.vue
@@ -0,0 +1,33 @@
+<script setup>
+import SizeSelector from './components/SizeSelector.vue'
+import { ref } from 'vue'
+const selectedSize = ref('S')
+</script>
+
+<template>
+  <h1>Seleccione la talla: {{ selectedSize }}</h1>
+  <SizeSelector
+    :selected-size="selectedSize"
+    @on-change-size="selectedSize = $event"
+  />
+</template>
+
+<style>
+#app {
+  font-family: Avenir, Arial, Helvetica, sans-serif;
+  text-align: center;
+}
+button {
+  padding: 5px;
+  margin-right: 5px;
+  border-radius: 5px;
+  border: none;
+  width: 75px;
+  cursor: pointer;
+}
+.active {
+  background-color: black;
+  color: white;
+  transition: all 0.3s ease-out;
+}
+</style>
---
+++ src/components/SizeSelector.vue
@@ -0,0 +1,20 @@
+<script setup>
+const possibleSizes = ['XS', 'S', 'M', 'L', 'XL', 'XXL']
+
+const props = defineProps({
+  selectedSize: String
+})
+
+const emits = defineEmits({
+  onChangeSize: String
+})
+</script>
+<template>
+  <button
+    v-for="size in possibleSizes"
+    :class="{ active: props.selectedSize === size }"
+    @click="emits('onChangeSize', size)"
+  >
+    {{ size }}
+  </button>
+</template>
